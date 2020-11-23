80e2f73c29dc7c25f50e2c25cfc4410b9e75fb38
James Manning <jamesmanningr98@gmail.com>
2020-11-23 11:22:55 +0000

    Made post vux actions async and added resets

--- src/store/modules/posts.js
+++ src/store/modules/posts.js
@@ -14,8 +14,14 @@ export const mutations = {
   SET_POSTS(state, posts) {
     state.posts = posts;
   },
+  RESET_POSTS(state) {
+    state.posts = {};
+  },
   SET_POST(state, post) {
     state.post = post;
+  },
+  RESET_POST(state) {
+    state.post = {};
   },
   UPDATE_POST(state, post) {
     const postIndex = state.posts.findIndex((item) => item.id == post._id);
@@ -28,55 +34,46 @@ export const mutations = {
 };
 
 export const actions = {
-  createPost({ commit }, post) {
-    return db
-      .postPost(post)
-      .then((post) => {
-        commit("ADD_POST", post);
-      })
-      .catch((err) => {
-        console.log(err.data.messege);
-      });
+  async createPost({ commit }, post) {
+    try {
+      const post_2 = await db.postPost(post);
+      commit("ADD_POST", post_2);
+      commit("SET_POST", {});
+    } catch (err) {
+      console.log(err.data.messege);
+    }
   },
-  fetchPosts({ commit }) {
-    return db
-      .getPosts()
-      .then((posts) => {
-        commit("SET_POSTS", posts);
-        return posts;
-      })
-      .catch((err) => {
-        console.log(err.data.messege);
-      });
+  async fetchPosts({ commit }) {
+    try {
+      const posts = await db.getPosts();
+      commit("SET_POSTS", posts);
+      return posts;
+    } catch (err) {
+      console.log(err.data.messege);
+    }
   },
-  fetchPost({ commit }, id) {
-    return db
-      .getPost(id)
-      .then((post) => {
-        commit("SET_POSTS", post);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  async fetchPost({ commit }, id) {
+    try {
+      const post = await db.getPost(id);
+      commit("SET_POSTS", post);
+    } catch (err) {
+      console.log(err);
+    }
   },
-  updatePost({ commit }, post) {
-    return db
-      .updatePost(post)
-      .then((post) => {
-        commit("UPDATE_POST", post);
-      })
-      .catch((err) => {
-        console.log(err.data.messege);
-      });
+  async updatePost({ commit }, post) {
+    try {
+      const post_2 = await db.updatePost(post);
+      commit("UPDATE_POST", post_2);
+    } catch (err) {
+      console.log(err.data.messege);
+    }
   },
-  deletePost({ commit }, post) {
-    return db
-      .deletePost(post._id)
-      .then((post) => {
-        commit("REMOVE_POST", [post._id]);
-      })
-      .catch((err) => {
-        console.log(err.data.messege);
-      });
+  async deletePost({ commit }, post) {
+    try {
+      const post_1 = await db.deletePost(post._id);
+      commit("REMOVE_POST", [post_1._id]);
+    } catch (err) {
+      console.log(err.data.messege);
+    }
   },
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,26 @@
 <template>
   <main>
-    <h1>Posts</h1>
-    <h2>Create a post</h2>
-    <post-edit-form v-model="post">
-      <v-btn
-        @click.prevent="$store.dispatch('post/createPost', post)"
-        color="primary"
-        slot="submit"
-        type="submit"
-      >
-        Create
-      </v-btn>
-    </post-edit-form>
-    <h2>Heres a list of posts</h2>
-    <ul>
-      <li v-for="post in posts" :key="post._id">
-        <h2>{{ post.title }}</h2>
-        <p>{{ post.body }}</p>
-      </li>
-    </ul>
+    <v-container>
+      <h1>Posts</h1>
+      <h2>Create a post</h2>
+      <post-edit-form v-model="post">
+        <v-btn
+          @click.prevent="createPost"
+          color="primary"
+          slot="submit"
+          type="submit"
+        >
+          Create
+        </v-btn>
+      </post-edit-form>
+      <h2>Heres a list of posts</h2>
+      <v-row>
+        <v-col cols="4" v-for="post in posts" :key="post._id">
+          <h2>{{ post.title }}</h2>
+          <p>{{ post.body }}</p>
+        </v-col>
+      </v-row>
+    </v-container>
   </main>
 </template>
 
@@ -42,6 +44,11 @@ export default {
   },
   computed: {
     ...mapState("post", ["posts"]),
+  },
+  methods: {
+    createPost() {
+      this.$store.dispatch("post/createPost", post);
+    },
   },
   created() {
     this.$store.dispatch("post/fetchPosts");
